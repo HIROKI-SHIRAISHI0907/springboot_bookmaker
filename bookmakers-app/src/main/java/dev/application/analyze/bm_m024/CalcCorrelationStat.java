547b2f1bbeeeb80edd098ec06efea8b4bfe9093d
shiraishi0907 <shiraishi0907github.com>
2025-07-24 00:51:25 +0900

    modi

--- bookmakers-app/src/main/java/dev/application/analyze/bm_m024/CalcCorrelationStat.java
+++ bookmakers-app/src/main/java/dev/application/analyze/bm_m024/CalcCorrelationStat.java
@@ -12,6 +12,8 @@ import java.util.stream.Collectors;
 
 import org.apache.commons.math3.stat.correlation.PearsonsCorrelation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import dev.application.analyze.bm_m023.AverageStatisticsSituationConst;
 import dev.application.analyze.bm_m023.ScoreBasedFeatureStat;
@@ -28,6 +30,8 @@ import dev.common.util.ExecuteMainUtil;
  * @author shiraishitoshio
  *
  */
+@Component
+@Transactional
 public class CalcCorrelationStat extends StatFormatResolver implements AnalyzeEntityIF {
 
 	/** プロジェクト名 */
