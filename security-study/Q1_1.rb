9aecd89f62feb9e46ed68496c8d7150433b11f67
Hiroki-Shiraishi813 <shiraishi-h@groove-gear.co.jp>
2025-07-24 12:40:00 +0900

    Q1,Q4 homework

---
+++ security-study/Q1_1.rb
@@ -0,0 +1,9 @@
+# config/initializers/cors.rb
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins "https://www.example.com"
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :options, :head]
+  end
+end
