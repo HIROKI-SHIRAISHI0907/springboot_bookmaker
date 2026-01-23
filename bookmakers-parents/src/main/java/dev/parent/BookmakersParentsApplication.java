6949914b7c6fcc8dbcb6dc0f4a62a368741cebd8
Hiroki-Shiraishi813 <shiraishi-h@groove-gear.co.jp>
2026-01-23 19:35:11 +0900

    modi

---
+++ bookmakers-parents/src/main/java/dev/parent/BookmakersParentsApplication.java
@@ -0,0 +1,13 @@
+package dev.parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmakersParentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmakersParentsApplication.class, args);
+	}
+
+}
---
+++ bookmakers-parents/src/test/java/dev/parent/BookmakersParentsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmakersParentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
