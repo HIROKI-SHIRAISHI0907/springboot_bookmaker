38a69bc67f1cd4013215a053d3ae6a3c0ab34f9b
Hiroki-Shiraishi813 <shiraishi-h@groove-gear.co.jp>
2026-01-30 18:52:40 +0900

    modi

--- bookmakers-app/src/main/java/dev/application/BookmakersAppApplication.java
+++ bookmakers-app/src/main/java/dev/application/BookmakersAppApplication.java
@@ -1,5 +1,6 @@
 package dev.application;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.ComponentScan;
 	    "dev.common",
 	    "dev.mng"
 	})
+@MapperScan("dev.application.domain")
 //@EnableScheduling
 public class BookmakersAppApplication {
 
