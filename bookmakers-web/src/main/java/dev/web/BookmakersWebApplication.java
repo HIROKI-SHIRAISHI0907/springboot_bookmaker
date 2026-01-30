5b91209df6506689f95841f58ec9a7347bb04f2b
Hiroki-Shiraishi813 <shiraishi-h@groove-gear.co.jp>
2026-01-30 19:54:11 +0900

    modi

--- bookmakers-web/src/main/java/dev/web/BookmakersWebApplication.java
+++ bookmakers-web/src/main/java/dev/web/BookmakersWebApplication.java
@@ -1,11 +1,9 @@
 package dev.web;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"dev.web", "dev.common"})
-@MapperScan("dev.web")
 public class BookmakersWebApplication {
 
 	public static void main(String[] args) {
