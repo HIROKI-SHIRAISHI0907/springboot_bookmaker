e719c061f97b682b4052f5222c28bf940ab4e6f6
Hiroki-Shiraishi813 <shiraishi-h@groove-gear.co.jp>
2026-01-30 19:37:33 +0900

    modi

--- bookmakers-web/src/main/java/dev/web/controller/StatOriginalController.java
+++ bookmakers-web/src/main/java/dev/web/controller/StatOriginalController.java
@@ -1,5 +1,6 @@
 package dev.web.controller;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,28 +10,31 @@ import org.springframework.web.bind.annotation.RestController;
 import dev.web.api.bm_u001.AdminStatOrigin;
 import dev.web.api.bm_u001.StatSizeFinalizeRequest;
 import dev.web.api.bm_u001.StatSizeFinalizeResponse;
-import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/admin")
-@RequiredArgsConstructor
 public class StatOriginalController {
 
-    private final AdminStatOrigin adminStatOrigin;
+	private final AdminStatOrigin adminStatOrigin;
 
-    /**
-     * 管理者による表示統計データ狭窄管理
-     */
-    @PostMapping("/stat-origin")
-    public ResponseEntity<StatSizeFinalizeResponse> setStatFinalize(@RequestBody StatSizeFinalizeRequest req) {
-        try {
-            StatSizeFinalizeResponse res = adminStatOrigin.executeAll(req);
-            return ResponseEntity.ok(res);
-        } catch (Exception e) {
-            StatSizeFinalizeResponse res = new StatSizeFinalizeResponse();
-            res.setResponseCode("500");
-            res.setMessage("サーバーエラーが発生しました。");
-            return ResponseEntity.internalServerError().body(res);
-        }
-    }
+	public StatOriginalController(
+			@Qualifier("adminStatOriginUseCaseImpl") AdminStatOrigin adminStatOrigin) {
+		this.adminStatOrigin = adminStatOrigin;
+	}
+
+	/**
+	 * 管理者による表示統計データ狭窄管理
+	 */
+	@PostMapping("/stat-origin")
+	public ResponseEntity<StatSizeFinalizeResponse> setStatFinalize(@RequestBody StatSizeFinalizeRequest req) {
+		try {
+			StatSizeFinalizeResponse res = adminStatOrigin.executeAll(req);
+			return ResponseEntity.ok(res);
+		} catch (Exception e) {
+			StatSizeFinalizeResponse res = new StatSizeFinalizeResponse();
+			res.setResponseCode("500");
+			res.setMessage("サーバーエラーが発生しました。");
+			return ResponseEntity.internalServerError().body(res);
+		}
+	}
 }
