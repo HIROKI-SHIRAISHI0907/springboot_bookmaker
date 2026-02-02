b3ff8a3ebd282d301577f686ddf193db42903068
Hiroki-Shiraishi813 <shiraishi-h@groove-gear.co.jp>
2026-02-02 13:27:36 +0900

    modi

---
+++ bookmakers-web/src/main/java/dev/web/controller/HealthController.java
@@ -0,0 +1,15 @@
+package dev.web.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1")
+public class HealthController {
+	@GetMapping("/healthz")
+	public ResponseEntity<String> healthz() {
+		return ResponseEntity.ok("ok");
+	}
+}
