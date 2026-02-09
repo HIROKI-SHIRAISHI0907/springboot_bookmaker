fad8fe74c8e8f3e90b0fb0a593477324370f791c
Hiroki-Shiraishi813 <shiraishi-h@groove-gear.co.jp>
2026-02-09 21:42:37 +0900

    modi

--- bookmakers-batch/src/main/java/dev/batch/bm_b001/B001AsyncMasterPythonWorker.java
+++ bookmakers-batch/src/main/java/dev/batch/bm_b001/B001AsyncMasterPythonWorker.java
@@ -14,6 +14,7 @@ import java.util.Map;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -129,6 +130,7 @@ public class B001AsyncMasterPythonWorker {
 
 	/** ジョブ実行管理IF */
 	@Autowired
+	@Qualifier("batchJobExecControl")
 	private jobExecControlIF jobExecControl;
 
 	/**
