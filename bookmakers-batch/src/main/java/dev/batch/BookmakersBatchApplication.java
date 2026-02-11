cb21b7432acd2959fc2d77dc205197dc7c5d4368
Hiroki-Shiraishi813 <shiraishi-h@groove-gear.co.jp>
2026-02-11 20:46:35 +0900

    modi

--- bookmakers-batch/src/main/java/dev/batch/BookmakersBatchApplication.java
+++ bookmakers-batch/src/main/java/dev/batch/BookmakersBatchApplication.java
@@ -1,11 +1,9 @@
 package dev.batch;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"dev.batch", "dev.common"})
-@MapperScan("dev.batch")
 public class BookmakersBatchApplication {
 
 	public static void main(String[] args) {
