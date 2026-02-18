299359bab0dd84c5d0879e02e1c38b5551c7e56c
Hiroki-Shiraishi813 <shiraishi-h@groove-gear.co.jp>
2026-02-19 02:31:49 +0900

    modi

--- bookmakers-app/src/main/java/dev/application/analyze/bm_m098/CsvSeqManageEntity.java
+++ bookmakers-app/src/main/java/dev/application/analyze/bm_m098/CsvSeqManageEntity.java
@@ -1,16 +1,25 @@
 package dev.application.analyze.bm_m098;
 
+import java.io.Serializable;
+
+import dev.common.entity.MetaEntity;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
-public class CsvSeqManageEntity {
+@EqualsAndHashCode(callSuper = false)
+public class CsvSeqManageEntity extends MetaEntity implements Serializable {
 
+	/** ID */
     private Integer id;
 
+    /** ジョブクラス名 */
     private String jobName;
 
+    /** 最終成功CSV番号 */
     private Integer lastSuccessCsv;
 
+    /** 取り戻し範囲 */
     private Integer backRange;
 
 }
