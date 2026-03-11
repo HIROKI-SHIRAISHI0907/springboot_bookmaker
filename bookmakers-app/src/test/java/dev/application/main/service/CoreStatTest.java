02e19697ac322e0e85c7e1a4e94c88f6d91f2c52
Hiroki-Shiraishi813 <shiraishi-h@groove-gear.co.jp>
2026-03-11 21:16:25 +0900

    modi

--- bookmakers-app/src/test/java/dev/application/main/service/CoreStatTest.java
+++ bookmakers-app/src/test/java/dev/application/main/service/CoreStatTest.java
@@ -8,11 +8,21 @@ import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import dev.common.entity.BookDataEntity;
 import dev.common.getinfo.GetStatInfo;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+	    "MASTER_DB_URL=jdbc:postgresql://localhost:54320/soccer_bm_master",
+	    "MASTER_DB_USER=postgres",
+	    "MASTER_DB_PASS=sonic3717",
+
+	    "BM_DB_URL=jdbc:postgresql://localhost:54320/soccer_bm",
+	    "BM_DB_USER=postgres",
+	    "BM_DB_PASS=sonic3717",
+	})
+@ActiveProfiles("prod")
 class CoreStatTest {
 
 	/**
@@ -30,6 +40,22 @@ class CoreStatTest {
 		// シーケンスデータから取得(最大値情報取得)
 		String csvNumber = "0";
 		String csvBackNumber = null;
+
+		// 直近のCSVデータ情報を取得
+		List<String> list = this.getStatInfo.listCsvKeysInRange(csvNumber, csvBackNumber);
+		Map<String, Map<String, List<BookDataEntity>>> entities = getStatInfo.getStatMapForSingleKey(list.get(0));
+		// Act
+		int result = statService.execute(entities);
+
+		// Assert
+		assertEquals(0, result); // 戻り値が0であること
+	}
+
+	@Test
+	void execute_test() throws Exception {
+		// シーケンスデータから取得(最大値情報取得)
+		String csvNumber = "4101";
+		String csvBackNumber = "4109";
 
 		// 直近のCSVデータ情報を取得
 		List<String> list = this.getStatInfo.listCsvKeysInRange(csvNumber, csvBackNumber);
