0074b395b43aa248de9df689b69998d5cad99936
Hiroki-Shiraishi813 <shiraishi-h@groove-gear.co.jp>
2026-06-12 00:50:11 +0900

    modi

---
+++ bookmakers-common-data/src/main/java/dev/common/constant/MasterNameConstant.java
@@ -0,0 +1,22 @@
+package dev.common.constant;
+
+/**
+ * マスタ名定数
+ * @author shiraishitoshio
+ *
+ */
+public class MasterNameConstant {
+
+	/** コンストラクタ生成禁止 */
+	private MasterNameConstant() {}
+
+	/** COUNTRY_LEAGUE_SEASON_MASTER */
+	public static final String COUNTRY_LEAGUE_SEASON_MASTER = "country_league_season_master";
+
+	/** COUNTRY_LEAGUE_MASTER */
+	public static final String COUNTRY_LEAGUE_MASTER = "country_league_master";
+
+	/** TEAM_COLOR_MASTER */
+	public static final String TEAM_COLOR_MASTER = "team_color_master";
+
+}
