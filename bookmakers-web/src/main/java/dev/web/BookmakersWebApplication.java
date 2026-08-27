a5a4070348812ed5a6b96d7243936d97dd8c1efb
Hiroki-Shiraishi813 <shiraishi-h@groove-gear.co.jp>
2026-08-27 22:37:31 +0900

    modi

--- bookmakers-web/src/main/java/dev/web/BookmakersWebApplication.java
+++ bookmakers-web/src/main/java/dev/web/BookmakersWebApplication.java
@@ -2,10 +2,8 @@ package dev.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication(scanBasePackages = {"dev.web", "dev.common"})
-@ConfigurationPropertiesScan(basePackages = {"dev.common.mail"})
 public class BookmakersWebApplication {
 
 	public static void main(String[] args) {
