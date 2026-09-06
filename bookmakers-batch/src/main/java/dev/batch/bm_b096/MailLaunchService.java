192dde790aac68ee0bd07ce3c8aa5c7d336f47b6
Hiroki-Shiraishi813 <shiraishi-h@groove-gear.co.jp>
2026-09-06 18:32:47 +0900

    modi

--- bookmakers-batch/src/main/java/dev/batch/bm_b096/MailLaunchService.java
+++ bookmakers-batch/src/main/java/dev/batch/bm_b096/MailLaunchService.java
@@ -192,12 +192,12 @@ public class MailLaunchService {
 			}
 			// valueを解決
 			if (BATCH_NAME_PLACEHOLDER.equals(key))
-				value = BatchCodeToMailEnum.resolveBatchName(value);
 				bikouDto.setBatchScrapeCd(value);
+				value = BatchCodeToMailEnum.resolveBatchName(value);
 
 			if (SCRAPE_NAME_PLACEHOLDER.equals(key))
-				value = ScrapeCodeToMailEnum.resolveScrapeName(value);
 				bikouDto.setBatchScrapeCd(value);
+				value = ScrapeCodeToMailEnum.resolveScrapeName(value);
 
 			result = result.replace("（" + key + "）", value);
 			result = result.replace("{{" + key + "}}", value);
