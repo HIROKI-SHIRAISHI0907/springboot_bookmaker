f88a7188a041a73ec96ac9bcc1c33fdff2aac994
Hiroki-Shiraishi813 <shiraishi-h@groove-gear.co.jp>
2026-09-14 23:10:41 +0900

    modi

--- bookmakers-web/src/main/java/dev/web/mail/MailSendService.java
+++ bookmakers-web/src/main/java/dev/web/mail/MailSendService.java
@@ -381,8 +381,9 @@ public class MailSendService {
 	 * @param mailId
 	 * @param batchScrapeCd
 	 * @param mailProcessKey
+	 * @throws Exception
 	 */
-	private void putJson(String mailId, String batchScrapeCd, String mailProcessKey) {
+	private void putJson(String mailId, String batchScrapeCd, String mailProcessKey) throws Exception {
 		putMailNoticeJson.putJson(MailConvertS3BucketUtil
 				.getS3Bucket(mailId, batchScrapeCd, null) + S3Const.JSON, mailProcessKey);
 	}
